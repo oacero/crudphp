eea8cfab139f08fd6155f010d7dd86efdf13ec6f
Oscar Acero <oskargiovanny@gmail.com>
2023-05-22 19:26:01 -0500

    First commit

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+// Config database params
+return [
+  'db' => [
+    'host' => 'localhost',
+    'user' => 'root',
+    'pass' => '',
+    'name' => 'tutorial_crud',
+    'options' => [
+      PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+    ]
+  ]
+];
